c0cbea68a752b5fc5c2c22896d04435464e7a238
Julian Rüth <julian.rueth@fsfe.org>
2019-08-05 08:04:15 +0200

    Create coverage for test files
    so header files show up in the output

--- recipe/coverage.sh
+++ recipe/coverage.sh
@@ -2,7 +2,7 @@
 set -exo pipefail
 
 # Create coverage statistics
-make check
+make CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS"
 
 # Install gcov
 conda install -y --quiet gcc_linux-64
@@ -17,6 +17,8 @@ git fetch origin
 git checkout -b master
 git branch -u origin/master
 
+find
+
 set +x
 # Do not upload; print what would have been uploaded instead…
 COVERALLS_FLAGS="-d"
@@ -25,4 +27,4 @@ if [ ${#COVERALLS_REPO_TOKEN} = 36 ];then
   COVERALLS_FLAGS="-t $COVERALLS_REPO_TOKEN"
 fi
 
-bash <(curl -s https://codecov.io/bash) $COVERALLS_FLAGS -R `pwd` -x `which x86_64-conda_cos6-linux-gnu-gcov` -a '\-lrp' -p ./libintervalxt/src
+bash <(curl -s https://codecov.io/bash) $COVERALLS_FLAGS -R `pwd` -x `which x86_64-conda_cos6-linux-gnu-gcov` -a '\-lrp' -p .
