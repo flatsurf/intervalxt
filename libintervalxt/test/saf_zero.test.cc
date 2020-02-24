6667d7bc126ca2dadb80972eb5bb55203a45f8f5
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2020-02-24 09:10:09 +0100

    example with infinite loop

--- libintervalxt/test/saf_zero.test.cc
+++ libintervalxt/test/saf_zero.test.cc
@@ -26,7 +26,6 @@
 
 #include "external/catch2/single_include/catch2/catch.hpp"
 
-#include "../intervalxt/induction_step.hpp"
 #include "../intervalxt/interval_exchange_transformation.hpp"
 #include "../intervalxt/label.hpp"
 #include "../intervalxt/connection.hpp"
@@ -35,8 +34,6 @@
 #include "../intervalxt/fmt.hpp"
 #include "../intervalxt/sample/e-antic-arithmetic.hpp"
 #include "../intervalxt/sample/lengths.hpp"
-#include "../intervalxt/sample/mpz-arithmetic.hpp"
-#include "../intervalxt/sample/rational-arithmetic.hpp"
 
 using eantic::renf_class;
 using eantic::renf_elem_class;
@@ -44,20 +41,25 @@ using Result = ::intervalxt::DecompositionStep::Result;
 
 namespace intervalxt::test {
 
-TEST_CASE("Cubic IET in H(3,1) with SAF=0") {
+TEST_CASE("Cubic IET in H(3,1) with SAF=0", "[saf_zero_interval_exchange_transformation]") {
   using namespace eantic;
   using EAnticLengths = sample::Lengths<renf_elem_class>;
-  auto K = renf_class::make("A^3 - 6*A^2 + 9*A - 3", "a", "3.87 +/- 0.01");
+  auto K = renf_class::make("A^3 - 6*A^2 + 9*A - 3", "A", "3.87 +/- 0.01");
   auto&& [lengths, a, b, c, d, e, f, g] = EAnticLengths::make(renf_elem_class(K, "-A^2 + 4*A"),
           renf_elem_class(K, "1/3*A^2 - A - 1"), renf_elem_class(K, "40/3*A^2 - 61*A + 36"),
           renf_elem_class(K, "-25/3*A^2 + 38*A - 22"), renf_elem_class(K, "1/3*A^2 - 5"),
           renf_elem_class(K, "-5*A^2 + 22*A - 10"), renf_elem_class(K, "1/3*A^2 - 2*A + 3"));
-  auto iet = IntervalExchangeTransformation(std::make_shared<Lengths>(lengths), {a, b, c, d, e, f, g}, {d, f, e, c, b, g, a});
+  auto iet = IntervalExchangeTransformation(std::make_shared<Lengths>(lengths), {a, b, c, d, e, f, g}, {g, f, d, c, e, b, a});
 
-  auto s = iet.safInvariant();
-  REQUIRE(s.min() == 0);
-  REQUIRE(s.max() == 0);
+  SECTION("SAF should be zero") {
+    auto s = iet.safInvariant();
+    REQUIRE(s.min() == 0);
+    REQUIRE(s.max() == 0);
+  }
 
   auto decomposition = DynamicalDecomposition(iet);
+  REQUIRE(decomposition.components().size() == 1);
+  auto component = decomposition.components()[0];
+  auto step0 = component.decompositionStep();
   }
-}
+}  // namespace intervalxt::test
